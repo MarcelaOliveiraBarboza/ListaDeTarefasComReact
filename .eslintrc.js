b44c26a002f525097f37d1bafd83d847ee57348a
Marcela Oliveira <marcela.oliveira052004@gmail.com>
2022-06-23 10:48:09 -0300

    Lista de Tarefas com React

---
+++ .eslintrc.js
@@ -0,0 +1,40 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    jest: true,
+    node: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:prettier/recommended',
+    'eslint-plugin-prettier'
+
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parser: '@babel/eslint-parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['react', 'prettier', 'react-hooks'],
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+  rules: {
+    "react/jsx-filename-extension": 0,
+    "react/state-in-constructor": 0,
+    "react/forbid-prop-types": 0,
+    "react/react-in-jsx-scope": 'off',
+  },
+};
